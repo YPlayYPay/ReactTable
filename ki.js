8b7edafb89ff9ae831c8ed5ba3384c587ca6b320
keaidaitou <1421732174@qq.com>
2016-10-11 17:45:36 +0800

    ggg

---
+++ ki.js
@@ -0,0 +1,11 @@
+/**
+ * Created by user2 on 2016-10-11.
+ */
+var data = [
+    {n1: '武汉大学', n2: 20},
+    {n1: '武汉大学', n2: 20}
+];
+for(var o in data[0]){
+    var length=o.length;
+};
+console.log(length)
--- server.js
+++ server.js
@@ -2,5 +2,5 @@
  * Created by user2 on 2016-10-09.
  */
 var data = [{
-    trial:'武汉大学实验aaaaaaaaaaaaa'
+    n1:'武汉大学实验aaaaaaaaaaaaa'
 }]
